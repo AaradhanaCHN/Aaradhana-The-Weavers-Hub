c4bd4cf94da6dcd9daac09feb366654b1658de19
AaradhanaCHN <97783694+AaradhanaCHN@users.noreply.github.com>
2022-01-15 14:46:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function cat1(){
+    localStorage.setItem("category", "1");
+    window.location="category.html"
+}
+function cat2(){
+    localStorage.setItem("category", "2");
+    window.location="category.html"
+}
+function cat3(){
+    localStorage.setItem("category", "3");
+    window.location="category.html"
+}
+function cat4(){
+    localStorage.setItem("category", "4");
+    window.location="category.html"
+}
+function cat5(){
+    localStorage.setItem("category", "5");
+    window.location="category.html"
+}
+function cat6(){
+    localStorage.setItem("category", "6");
+    window.location="category.html"
+}
+function cat7(){
+    localStorage.setItem("category", "7");
+    window.location="category.html"
+}
+function cat8(){
+    localStorage.setItem("category", "8");
+    window.location="category.html"
+}
+function cat9(){
+    localStorage.setItem("category", "9");
+    window.location="category.html"
+}
+function cat10(){
+    localStorage.setItem("category", "10");
+    window.location="category.html"
+}
+function catBoot(){
+    document.getElementById("cat").innerHTML=localStorage.getItem("category");
+}
+/* Copyright Aaradhana Chennai 2022 */
